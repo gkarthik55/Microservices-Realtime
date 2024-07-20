5bc38c1eb79d6c31a29455fe26b0a90ad215d212
gkarthik05 <gkarthik.55@gmail.com>
2024-07-20 21:09:31 +0530

    Implemented Resilience4j Circuit Breaker

--- order-service/src/main/java/com/spring/os/controller/OrderController.java
+++ order-service/src/main/java/com/spring/os/controller/OrderController.java
@@ -16,6 +16,8 @@ import com.spring.os.entity.Orders;
 import com.spring.os.repository.OrderRepository;
 import com.spring.os.service.OrdersService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/orders")
 public class OrderController 
@@ -32,11 +34,26 @@ public class OrderController
 		return new ResponseEntity<>("hello from orders service", HttpStatus.OK);
 	}
 	
+	public static final String ORDER_SERVICE="OrderService";
+	
 	@PostMapping("/add")
+	@CircuitBreaker(name=ORDER_SERVICE, fallbackMethod="addOrderFailureHandle")
 	public ResponseEntity<TransactionResponse> addOrder(@RequestBody TransactionRequest request)
 	{
 		return ordersService.addOrder(request);
 	}
+	
+	// Fallback method signature should match the original method, plus Throwable
+	public ResponseEntity<TransactionResponse> addOrderFailureHandle(Exception e)
+	{
+		String failureMessage = "There is some issue in the Payment Gateway. Please try again later.";
+		
+		TransactionResponse response = new TransactionResponse();
+		response.setMessage(failureMessage);
+		
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+	
 	
 	@GetMapping("/getAllOrders")
 	public ResponseEntity<java.util.List<Orders>> getAllOrders()
